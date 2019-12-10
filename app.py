ce0a7e5ebff3e00c5850912f5b723cb13e15355d
daddyawesome <sablay296@gmail.com>
2019-12-10 12:52:44 +0000

    added model to database

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ import os
 from flask import Flask
 from flask import render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default =datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
