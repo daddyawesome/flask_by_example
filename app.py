c5a9caaef3f8431b47b0b650e08a003a31411892
daddyawesome <sablay296@gmail.com>
2019-12-10 15:36:01 +0000

    adding app route delete

--- app.py
+++ app.py
@@ -35,6 +35,17 @@ def index():
         
     
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
 
 
 @app.route('/<name>')
